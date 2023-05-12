9c796c71c55658243736d9fa47c2e46f8deb8546
rafaeldfigueiredo <rafaeldfigueiredo@hotmail.com>
2023-05-12 15:14:01 +0000

    start usability edits

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,15 @@
-let styleButton = document.querySelector(".styleButton")
+function boldIt() {
+    if (document.querySelector('.bold').onclick) {
+        if (document.querySelector('textArea').onselect)  {
+            document.querySelector('textArea').style.fontWeight = 'bold';
+        }
+    }
+}
 
-function myFunc(){
-    alert(styleButton.textContent)
+function italicIt() {
+    document.querySelector('textArea').style.fontStyle = 'italic';
+}
+
+function underlineIt() {
+    document.querySelector('textArea').style.textDecoration = 'underline';
 }
