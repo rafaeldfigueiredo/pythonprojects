7942fad1a253c5fbf8066d4a622cfeb676f70561
rafaeldfigueiredo <rafaeldfigueiredo@hotmail.com>
2023-05-15 15:09:12 +0000

    add edits

--- static/js/app.js
+++ static/js/app.js
@@ -1,13 +1,13 @@
 let textArea = document.querySelector('#textArea');
-
-function boldIt() {
-    textArea.setAttribute("style", "font-weight: bold");
-}
+let boldButton = document.querySelector('.bold');
+let italicButton = document.querySelector('.italic');
+let underlineButton = document.querySelector('underline');
 
 function italicIt() {
-    textArea.setAttribute("style", "font-style: italic");
+    if (textArea.setAttribute("style", "font-style: italic")) {
+        textArea.setAttribute("style", "font-style: italic");
+    }
 }
-
 function underlineIt() {
     textArea.setAttribute("style", "text-decoration: underline");
 }
