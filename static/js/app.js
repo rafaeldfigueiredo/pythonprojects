9037f4017b6bb1e30340913a3cc74b4f97ffc993
rafaeldfigueiredo <rafaeldfigueiredo@hotmail.com>
2023-05-18 12:33:19 +0000

    edit files, broken underline style

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,8 @@
 let textArea = document.querySelector('#textArea');
-let selected = document.getSelection.toString();
+let boldButton = document.querySelector('.bold');
+let italicButton = document.querySelector('.italic');
+let underlineButton = document.querySelector('.underline')
+var selected = textArea.value.substring(textArea.selectionStart, textArea.selectionEnd);
 
 function boldIt() {
     textArea.style.fontWeight == "bold" ? textArea.style.fontWeight = "normal" : textArea.style.fontWeight = "bold"
