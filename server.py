61ac48928b86e072f2a2b4f7c6d1cf48b3580702
rafaeldfigueiredo <rafaeldfigueiredo@hotmail.com>
2023-05-22 13:36:02 +0000

    made file proto struct

--- server.py
+++ server.py
@@ -2,7 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
+@app.route("/landpage")
+def landPage():
+    return render_template("landpage.html")
+
+
 @app.route("/textEditor")
 def textEditor():
     return render_template("textEditor.html")
@@ -11,5 +17,9 @@ def textEditor():
 @app.route("/bin2dec")
 def bin2dec():
     return render_template("bin2dec.html")
+
+@app.route("/makeText")
+def makeText():
+    return render_template("makeText.html")
 
 app.run(debug=True)
