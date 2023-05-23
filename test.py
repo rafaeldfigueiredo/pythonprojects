b62e55420acad586d4aa1cebdcd33dbedef50c38
rafaeldfigueiredo <rafaeldfigueiredo@hotmail.com>
2023-05-23 15:11:21 +0000

    started factory pattern

---
+++ test.py
@@ -0,0 +1,11 @@
+class Pessoa:
+    def __init__(self,nome,sobrenome):
+        self.nome = nome
+        self.sobrenome = sobrenome
+
+    def criarPessoa():
+        return f"{self.nome} {self.sobrenome}"
+    
+    pessoa = {"nome":self.nome, "sobrenome":self.sobrenome, "criarPessoa":self.criarPessoa}
+    
+    return pessoa
