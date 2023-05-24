d5ccaaa8bdf99b6af865ba8475d5ecca79c5e61b
rafaeldfigueiredo <rafaeldfigueiredo@hotmail.com>
2023-05-24 15:13:09 +0000

    testing proto

--- test.py
+++ test.py
@@ -1,11 +1,10 @@
+import re
 class Pessoa:
-    def __init__(self,nome,sobrenome):
-        self.nome = nome
-        self.sobrenome = sobrenome
-
-    def criarPessoa():
-        return f"{self.nome} {self.sobrenome}"
-    
-    pessoa = {"nome":self.nome, "sobrenome":self.sobrenome, "criarPessoa":self.criarPessoa}
-    
-    return pessoa
+    def __init__(self, nome=input("Digite seu nome:\n"), sobrenome=input("Digite seu sobrenome:\n")):
+        #Takes out numbers and symbols from names.
+        rgx = r'[^a-zA-Z]'
+        self.nome = re.sub(rgx, '', nome)
+        self.sobrenome = re.sub(rgx, '', sobrenome)
+    def criarPessoa(self):
+        print(f"{self.nome} {self.sobrenome}, você está cadastrado")
+pessoa = Pessoa().criarPessoa() #Instantly instantiate the class object
